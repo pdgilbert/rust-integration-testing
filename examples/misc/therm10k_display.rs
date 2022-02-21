e528e9886bb8352f0059a79dfe94559a95739597
Paul Gilbert <pdgilbert@users.noreply.github.com>
2022-02-21 10:44:14 -0500

    minor cleanup.

--- examples/misc/therm10k_display.rs
+++ examples/misc/therm10k_display.rs
@@ -1,25 +1,29 @@
 // WORK IN PROGRESS
-//! Measure temperature with 10k thermistor sensor and display on SSD1306 OLED display.
-//! See misc/temperature.rs and misc/temperature_display.rs forother sensors and 
+//! Measure temperature with 10k thermistor sensor (NTC thermistors probe ) and display on SSD1306 OLED display.
+//! One side of the thermistor is connected to GND and other side to adc pin and also through
+//! a 10k sresistor to VCC. That makes the max voltage about VCC/2, so about 2.5v when VCC is 5v.
+//! This is convenient adc for pins that are not 5v tolerant but means the voltage varies
+//! inversely to connecting throught the resistor to GND as is sometimes done. (That is,
+//! higher temperature gives lower voltage measurement.
+//! See setup() functions in  src/adc_i2c_led_delay.rs, src/i2c.rs, and src/led.rs  for the 
+//! pin settings on various boards.
+//! 
+//! See misc/temperature.rs and misc/temperature_display.rs for other sensors and 
 //! the internal mcu temperature measurement.
 //! 
 //! Note that the DisplaySize setting needs to be adjusted for 128x64 or 128x32 display.
 //!
-//! The 10K thermistor voltage is measured with      analog/digital converter.
-//!
-//! The setup() functions make the application code common. They are in src/i2c_led_delay.rs.
-//!
-//! On "BluePill" (stm32f1xx_hal) the I2C1 connections are SDA on PB9, SCL on PB8. ADC as below.
-//! See src/i2c_led_delay.rs for settings on other boards. The section of setup() corresponding 
-//! to the HAL setting specifies details of pin connections.
 //! If 3.3v is supplied through BluePill from 5v BEWARE of regulator limit.
 //! 
 //! The SSD1306 OLED display connects to the I2C bus: VCC  (or VDD) to 3.3v, and also to GND, SDA, and SCL. 
 //! 
-//! The 10K thermistor connects to 3.3v and is in series with a 10K fixed resistor which connects to GND.
-//! (see for example https://www.jameco.com/Jameco/workshop/TechTip/temperature-measurement-ntc-thermistors.html
+//! Voltage at the thermistor to fixed resistor connection is measured on the ADC pin.
+//! Te choice of 10k series resistor in the voltage divider ...
+//! Values in the temperature calculation below are very rough based on my extremely crude calibration
+//! effort, but see for example 
+//!     https://www.mathscinotes.com/2014/05/yet-another-thermistor-discussion/
+//!  or https://www.jameco.com/Jameco/workshop/TechTip/temperature-measurement-ntc-thermistors.html
 //!  or  https://learn.adafruit.com/thermistor/using-a-thermistor
-//! Voltage at the thermistor to fixed resistor connection is measured on the ADC pin.  
 
 
 #![deny(unsafe_code)]
@@ -32,8 +36,11 @@ use embedded_hal::blocking::delay::DelayMs;
 //use nb::block;
 
 use core::fmt::Write;
+
+// Note hprintln will not run without an ST-link probe (eg. not on battery power)
+// The use statement does not need to be removed, but it will be unused and causes warnings.
+//use cortex_m_semihosting::hprintln;
 //use rtt_target::{rprintln, rtt_init_print};
-use cortex_m_semihosting::hprintln;
 
 // See https://docs.rs/embedded-graphics/0.7.1/embedded_graphics/mono_font/index.html
 // DisplaySize128x32:
@@ -55,8 +62,8 @@ use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306, mode::BufferedGraphicsMo
 use rust_integration_testing_of_examples::adc_i2c_led_delay::{setup, LED, ReadAdc};
 
 fn show_display<S>(
-    thermistor: u32,
-    temp: u32,
+    mv: u32,
+    temp: i64,
     text_style: MonoTextStyle<BinaryColor>,
     disp: &mut Ssd1306<impl WriteOnlyDataCommand, S, BufferedGraphicsMode<S>>,
 ) -> ()
@@ -71,8 +78,8 @@ where
 
     // Many SSD1306 modules have a yellow strip at the top of the display, so first line may be yellow.
     // it is now possible to use \n in place of separate writes, with one line rather than vector.
-    write!(lines[0], "thermistor{:3} C", thermistor).unwrap();
-    write!(lines[1], "temp:    {:5} C", temp).unwrap();
+    write!(lines[0], "probe {:3}mV {:5} C", mv, temp).unwrap();
+    //write!(lines[1], "temp:    {:5} C", temp).unwrap();
 
     disp.clear();
     for i in 0..lines.len() {
@@ -94,7 +101,7 @@ where
 fn main() -> ! {
     //rtt_init_print!();
     //rprintln!("therm10k_display example");
-    hprintln!("therm10k_display example").unwrap();
+    //hprintln!("therm10k_display example").unwrap();
 
     let (mut sens, i2c, mut led, mut delay) = setup();
 
@@ -113,20 +120,23 @@ fn main() -> ! {
         .text_color(BinaryColor::On)
         .build();
 
+    // crude linear aproximation
+    // t = a - v/b , t degrees C, v in mV, b negative of inverse slope, a includes degrees K to C
+    let a = 75i64;
+    let b = 40u32; 
 
     loop {
         // Blink LED  to check that loop is actually running.
         led.blink(50_u16, &mut delay);
 
         // Read adc
-        let thermistor = sens.read_mv();
-        //let thermistor = block!(sens.read_mv()).unwrap_or(8091) * 5;  //FIX
-        let temp = thermistor;  //FIX formula
+        let mv = sens.read_mv();
+        //hprintln!("values read").unwrap();
+        let temp:i64  = a - (mv / b) as i64 ;
 
-    hprintln!("values read").unwrap();
-    hprintln!("values  {} {} ", thermistor, temp).unwrap();
+        //hprintln!("probe  {}mV  {}C ", mv, temp).unwrap();
 
-        show_display(thermistor, temp, text_style, &mut display);
+        show_display(mv, temp, text_style, &mut display);
 
         delay.delay_ms(5000_u16);
     }
