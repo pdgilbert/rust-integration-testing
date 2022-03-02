7d28f39ac8291e4abc93c02a4f490c55dccf1449
Paul Gilbert <pdgilbert@users.noreply.github.com>
2022-03-02 17:28:08 -0500

    adjustments for stm32f4xx_hal api change.

--- src/i2c.rs
+++ src/i2c.rs
@@ -171,7 +171,8 @@ use stm32f4xx_hal::{
 
 
 #[cfg(feature = "stm32f4xx")]
-pub type I2c1Type = I2c<I2C1, (PB8<Alternate<OpenDrain, 4u8>>, PB9<Alternate<OpenDrain, 4u8>>)>;
+pub type I2c1Type = I2c<I2C1, (PB8<Alternate<4u8, OpenDrain>>, PB9<Alternate<4u8, OpenDrain>>)>;
+//pub type I2c1Type = I2c<I2C1, (PB8<Alternate<OpenDrain, 4u8>>, PB9<Alternate<OpenDrain, 4u8>>)>;
 //pub type I2c1Type =  I2c<I2C1, impl Pins<I2C1>>;
 
 #[cfg(feature = "stm32f4xx")]
@@ -181,12 +182,11 @@ pub fn setup_i2c1(i2c1: I2C1, gpiob: PartsB, &clocks: &Clocks) -> I2c1Type {
     let sda = gpiob.pb9.into_alternate_open_drain(); 
 
     let i2c = I2c::new(i2c1, (scl, sda), 400.kHz(), &clocks);
-
     i2c
 }
 
 #[cfg(feature = "stm32f4xx")]
-pub type I2c2Type = I2c<I2C2, (PB10<Alternate<OpenDrain, 4u8>>, PB3<Alternate<OpenDrain, 9u8>>)>;
+pub type I2c2Type = I2c<I2C2, (PB10<Alternate<4u8, OpenDrain>>, PB3<Alternate<9u8, OpenDrain>>)>;
 //pub type I2c2Type =   I2c<I2C2, impl Pins<I2C2>>;   
 
 #[cfg(feature = "stm32f4xx")]
