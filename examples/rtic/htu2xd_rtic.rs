36795a97615775ccb1c2776478115e8daf8a1033
Paul Gilbert <pdgilbert@users.noreply.github.com>
2023-01-23 00:05:41 -0400

    added htu2xd_rtic example.

---
+++ examples/rtic/htu2xd_rtic.rs
@@ -0,0 +1,244 @@
+//! NOT Jan, 2023 - This is working with USB probe and with battery. Run tested on bluepill. 
+//! NOT            It has a  workaround for SSD1306  text_style.
+//! 
+//! Note that led and i2c pin settings are specific to a board pin configuration used for testing,
+//! despite the cfg feature flags suggesting it may be for a HAL.
+//! 
+//! Measure the temperature and humidity from an htu2xd and display on OLED with shared bus i2c1.
+//! Compare examples dht_rtic, htu2xd_displau.
+//! Blink (onboard) LED with short pulse evry read.
+//! On startup the LED is set on for a second in the init process.
+//! One main processe is scheduled. It reads the sensor and spawns itself to run after a delay.
+//! It also spawns a `blink` process that turns the led on and schedules another process to turn it off.
+//!
+//!  A good reference on performance of humidity sensors is
+//!     https://www.kandrsmith.org/RJS/Misc/hygrometers.html
+
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+#[cfg(debug_assertions)]
+use panic_semihosting as _;
+
+#[cfg(not(debug_assertions))]
+use panic_halt as _;
+
+use rtic::app;
+
+#[cfg_attr(feature = "stm32f0xx", app(device = stm32f0xx_hal::pac,   dispatchers = [TIM3]))]
+#[cfg_attr(feature = "stm32f1xx", app(device = stm32f1xx_hal::pac,   dispatchers = [TIM2, TIM3]))]
+#[cfg_attr(feature = "stm32f3xx", app(device = stm32f3xx_hal::pac,   dispatchers = [TIM2, TIM3]))]
+#[cfg_attr(feature = "stm32f4xx", app(device = stm32f4xx_hal::pac,   dispatchers = [TIM2, TIM3]))]
+#[cfg_attr(feature = "stm32f7xx", app(device = stm32f7xx_hal::pac,   dispatchers = [TIM2, TIM3]))]
+#[cfg_attr(feature = "stm32h7xx", app(device = stm32h7xx_hal::pac,   dispatchers = [TIM2, TIM3]))]
+#[cfg_attr(feature = "stm32l0xx", app(device = stm32l0xx_hal::pac,   dispatchers = [TIM2, TIM3]))]
+#[cfg_attr(feature = "stm32l1xx", app(device = stm32l1xx_hal::stm32, dispatchers = [TIM2, TIM3]))]
+#[cfg_attr(feature = "stm32l4xx", app(device = stm32l4xx_hal::pac,   dispatchers = [TIM2, TIM3]))]
+
+mod app {
+    use htu2xd::{Htu2xd, Reading};   //, Resolution
+
+    // Note that hprintln is for debugging with usb probe and semihosting. It causes battery operation to stall.
+    //use cortex_m_semihosting::{debug, hprintln};
+    //use cortex_m_semihosting::{hprintln};
+    
+    use core::fmt::Write;
+
+    use systick_monotonic::*;
+
+    // secs() and millis() methods from https://docs.rs/fugit/latest/fugit/trait.ExtU32.html#tymethod.secs
+    use fugit::TimerDuration;
+
+
+    // See https://docs.rs/embedded-graphics/0.7.1/embedded_graphics/mono_font/index.html
+    // DisplaySize128x32:
+    //    &FONT_6X10 128 pixels/ 6 per font = 21.3 characters wide.  32/10 = 3.2 characters high
+    //    &FONT_5X8  128 pixels/ 5 per font = 25.6 characters wide.  32/8  =  4  characters high
+    //    FONT_8X13  128 pixels/ 8 per font = 16   characters wide.  32/13 = 2.5 characters high
+    //    FONT_9X15  128 pixels/ 9 per font = 14.2 characters wide.  32/15 = 2.  characters high
+    //    FONT_9X18  128 pixels/ 9 per font = 14.2 characters wide.  32/18 = 1.7 characters high
+    //    FONT_10X20 128 pixels/10 per font = 12.8 characters wide.  32/20 = 1.6 characters high
+    
+    use embedded_graphics::{
+        //mono_font::{ascii::FONT_10X20, MonoTextStyleBuilder, MonoTextStyle}, 
+        mono_font::{iso_8859_1::FONT_10X20, MonoTextStyleBuilder}, 
+        pixelcolor::BinaryColor,
+        prelude::*,
+        text::{Baseline, Text},
+    };
+
+    use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
+
+    const MONOTICK:  u32 = 100;
+    const READ_INTERVAL: u64 = 2;  // used as seconds
+
+    const BLINK_DURATION: u64 = 20;  // used as milliseconds
+
+    use rust_integration_testing_of_examples::i2c1_i2c2_led_delay::{
+        setup_i2c1_i2c2_led_delay_using_dp, I2c1Type, LED, LedType, DelayType, DelayMs, MONOCLOCK};
+
+    use shared_bus::{I2cProxy};
+    use core::cell::RefCell;
+    use cortex_m::interrupt::Mutex;
+
+    fn show_display<S>(
+        temperature: f32,   // 10 * deg C to give one decimal place
+        relative_humidity: f32,
+        //text_style: MonoTextStyle<BinaryColor>,
+        disp: &mut Ssd1306<impl WriteOnlyDataCommand, S, BufferedGraphicsMode<S>>,
+    ) -> ()
+    where
+        S: DisplaySize,
+    {
+       
+       // workaround. build here because text_style cannot be shared
+       let text_style = MonoTextStyleBuilder::new().font(&FONT_10X20).text_color(BinaryColor::On).build();
+    
+       let mut lines: [heapless::String<32>; 1] = [
+           heapless::String::new(),
+       ];
+    
+       // Many SSD1306 modules have a yellow strip at the top of the display, so first line may be yellow.
+       // It is possible to use \n in place of separate writes, with one line rather than vector.
+    
+       // UTF-8 text is 2 bytes (2 ascii characters) in strings like the next. Cutting an odd number of character from
+       // the next test_text can result in a build error message  `stream did not contain valid UTF-8` even with
+       // the line commented out!! The test_txt is taken from 
+       //      https://github.com/embedded-graphics/examples/blob/main/eg-0.7/examples/text-extended-characters.rs
+       
+       //let test_text  = "¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
+       //   degree symbol "°" is about                  ^^ here 
+       
+       write!(lines[0], "{:.1}°C {:.0}% RH", temperature, relative_humidity).unwrap();
+
+       disp.clear();
+       for i in 0..lines.len() {
+           // start from 0 requires that the top is used for font baseline
+           Text::with_baseline(
+               &lines[i],
+               Point::new(0, i as i32 * 12), //with font 6x10, 12 = 10 high + 2 space
+               text_style,
+               Baseline::Top,
+               )
+               .draw(&mut *disp)
+               .unwrap();
+       }
+       disp.flush().unwrap();
+       ()
+    }
+
+
+    #[monotonic(binds = SysTick, default = true)]
+    type MyMono = Systick<MONOTICK>;
+
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        //rtt_init_print!();
+        //rprintln!("htu2xd_rtic example");
+        //hprintln!("htu2xd_rtic example").unwrap();
+
+        //let mut led = setup(cx.device);
+        let (i2c1, _i2c2, mut led, mut delay) = setup_i2c1_i2c2_led_delay_using_dp(cx.device);
+
+        led.on();
+        delay.delay_ms(1000u32);  
+        led.off();
+
+        let manager: &'static _ = shared_bus::new_cortexm!(I2c1Type = i2c1).unwrap();
+        let interface = I2CDisplayInterface::new(manager.acquire_i2c());
+
+        let text_style = MonoTextStyleBuilder::new().font(&FONT_10X20).text_color(BinaryColor::On).build();
+
+        //common display sizes are 128x64 and 128x32
+        let mut display = Ssd1306::new(interface, DisplaySize128x32, DisplayRotation::Rotate0)
+            .into_buffered_graphics_mode();
+
+        display.init().unwrap();
+
+        Text::with_baseline("Display initialized ...", Point::zero(), text_style, Baseline::Top, )
+          .draw(&mut display).unwrap();
+
+        // Start the sensor.
+        let mut sensor    = Htu2xd::new();
+        let mut htu_ch = manager.acquire_i2c();
+        sensor.soft_reset(&mut htu_ch).expect("sensor reset failed");
+        delay.delay_ms(15u32);     // Wait for the reset to finish
+
+        //    .read_user_register() dos not return and changes something that requires sensot power off/on.
+        //    let mut register = htu.read_user_register(&mut htu_ch).expect("htu.read_user_register failed");
+        //    register.set_resolution(Resolution::Humidity10Temperature13);   //.expect("set_resolution failed");
+        //    htu.write_user_register(&mut htu_ch, register).expect("write_user_register failed");
+
+        let mono = Systick::new(cx.core.SYST,  MONOCLOCK);
+
+        read_and_display::spawn().unwrap();
+
+        (Shared { led, },   Local {display, sensor, htu_ch }, init::Monotonics(mono))
+    }
+
+    #[shared]
+    struct Shared {
+        led:   LedType,      //impl LED, would be nice
+    }
+
+    #[local]
+    struct Local {
+        display:  Ssd1306<I2CInterface<I2cProxy<'static, Mutex<RefCell<I2c1Type>>>>, 
+                          ssd1306::prelude::DisplaySize128x32, 
+                          BufferedGraphicsMode<DisplaySize128x32>>,
+//        text_style: MonoTextStyle<BinaryColor>,
+//        sensor:  AHT10<shared_bus::I2cProxy<'static, NullMutex<I2c1Type>>, 
+//               stm32f1xx_hal::timer::Delay<stm32f1xx_hal::pac::TIM2, 1000000_u32>>,
+        sensor:  htu2xd::Htu2xd<shared_bus::I2cProxy<'static,  Mutex<RefCell<I2c1Type>>>>,
+        htu_ch:  I2cProxy<'static, Mutex<RefCell<I2c1Type>>>,
+    }
+
+    //#[task(shared = [led, delay, dht, text_style, display], capacity=2)]
+    #[task(shared = [led, ], local = [sensor, display, htu_ch ], capacity=2)]
+    fn read_and_display(cx: read_and_display::Context) {
+        blink::spawn(BLINK_DURATION.millis()).ok();
+
+        //let delay = cx.local.delay;
+        let sensor = cx.local.sensor;
+        let htu_ch = cx.local.htu_ch;
+
+        let z = sensor.read_temperature_blocking(htu_ch);
+        let t = match z {
+            Ok(Reading::Ok(t))     => t.as_degrees_celsius(),
+            Ok(Reading::ErrorLow)  => 409.0,
+            Ok(Reading::ErrorHigh) => 409.1,
+            Err(_)                 => 409.2,
+        };
+
+        let z = sensor.read_humidity_blocking(htu_ch);
+        let h = match z {
+            Ok(Reading::Ok(t))     => t.as_percent_relative(),
+            Ok(Reading::ErrorLow)  => 409.0,
+            Ok(Reading::ErrorHigh) => 409.1,
+            Err(_)                 => 409.,
+        };
+
+        show_display(t, h, cx.local.display);
+        read_and_display::spawn_after(READ_INTERVAL.secs()).unwrap();
+    }
+
+    #[task(shared = [led], capacity=2)]
+    fn blink(_cx: blink::Context, duration: TimerDuration<u64, MONOTICK>) {
+        // note that if blink is called with ::spawn_after then the first agument is the after time
+        // and the second is the duration.
+        //hprintln!("blink {}", duration).unwrap();
+        crate::app::led_on::spawn().unwrap();
+        crate::app::led_off::spawn_after(duration).unwrap();
+    }
+
+    #[task(shared = [led], capacity=2)]
+    fn led_on(mut cx: led_on::Context) {
+        cx.shared.led.lock(|led| led.on());
+    }
+
+    #[task(shared = [led], capacity=2)]
+    fn led_off(mut cx: led_off::Context) {
+        cx.shared.led.lock(|led| led.off());
+    }
+}
