a564cc7c0336f400fdc2c73f7cd1bf502084bfc2
Paul Gilbert <pdgilbert@users.noreply.github.com>
2023-04-14 00:11:03 -0300

    adding src/ files i2c1_i2c2_led_delays.rs and setups.rs.

---
+++ src/setups.rs
@@ -0,0 +1,8 @@
+// provide a common place to use  rust_integration_testing_of_examples::setups;
+
+pub use crate::delay::*;
+pub use crate::led::*;
+pub use crate::i2c::*;
+pub use crate::dp::{Peripherals};
+
+pub use crate::i2c1_i2c2_led_delays::*;
