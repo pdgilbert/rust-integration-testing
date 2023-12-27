ad3dd3012d4ce0ed5cff31e9c9d4a6ab6c3c37eb
Paul Gilbert <pdgilbert@users.noreply.github.com>
2023-12-26 23:46:51 -0400

    add new modules, ongoing refactor.

---
+++ src/cp.rs
@@ -0,0 +1,34 @@
+
+    #[cfg(feature = "stm32f0xx")]
+    pub use stm32f0xx_hal::pac::{CorePeripherals};
+
+    #[cfg(feature = "stm32f1xx")]
+    pub use stm32f1xx_hal::pac::{CorePeripherals};
+    
+    #[cfg(feature = "stm32f3xx")] //  eg Discovery-stm32f303
+    pub use stm32f3xx_hal::pac::{CorePeripherals};
+    
+    #[cfg(feature = "stm32f4xx")]
+    pub use stm32f4xx_hal::pac::{CorePeripherals};
+    
+    #[cfg(feature = "stm32f7xx")]
+    pub use stm32f7xx_hal::pac::{CorePeripherals};
+      
+    #[cfg(feature = "stm32g0xx")]
+    pub use stm32g0xx_hal::pac::{CorePeripherals};
+     
+    #[cfg(feature = "stm32g4xx")]
+    pub use stm32g4xx_hal::stm32::{CorePeripherals};
+  
+    #[cfg(feature = "stm32h7xx")]
+    pub use stm32h7xx_hal::pac::{CorePeripherals};
+    
+    #[cfg(feature = "stm32l0xx")]
+    pub use stm32l0xx_hal::pac::{CorePeripherals};
+        
+    #[cfg(feature = "stm32l1xx")] // eg  Discovery STM32L100 and Heltec lora_node STM32L151CCU6
+    pub use stm32l1xx_hal::stm32::{CorePeripherals};
+    
+    #[cfg(feature = "stm32l4xx")]
+    pub use stm32l4xx_hal::pac::{CorePeripherals};
+    
