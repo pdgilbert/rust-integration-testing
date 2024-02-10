a19c76815dfc2f89e0b454702554273e93f62eee
Paul Gilbert <pdgilbert@users.noreply.github.com>
2024-02-09 22:08:25 -0400

    removed bus sharing from example tcs34725-color-display.

--- examples/driver-examples/tcs34725-color-display.rs
+++ examples/driver-examples/tcs34725-color-display.rs
@@ -5,13 +5,15 @@
 //!  The specific function used will depend on the HAL setting (see README.md).
 //!  See the section of setup() corresponding to the HAL setting for details on pin connections.
 //!
-//!  On "BluePill" (stm32f1xx_hal) using I2C1.
+//!  On "BlackPill" (stm32f4xx_hal) using I2C1 and I2C2. See file src/i2c.rs for pin settings.
 //! ```
-//! BP   <-> TCS34725 <-> Display
-//! GND  <-> GND      <-> GND
-//! 3.3V <-> VCC      <-> VDD
-//! PB8  <-> SCL      <-> SCL
-//! PB9  <-> SDA      <-> SDA
+//! BP   <-> iAQ-Core-C <-> Display
+//! GND  <-> GND        <-> GND
+//! 3.3V <-> VCC        <-> VDD
+//! PB8  <->            <-> SCL1
+//! PB9  <->            <-> SDA1
+//! PB10 <-> SCL      
+//! PB3  <-> SDA       
 //! ```
 
 #![deny(unsafe_code)]
@@ -47,10 +49,9 @@ fn main() -> ! {
 
     let dp = Peripherals::take().unwrap();
 
-    let (i2c, _i2c2, mut led, mut delay, _clock) = i2c1_i2c2_led_delay::setup_from_dp(dp);
+    let (i2c1, i2c2, mut led, mut delay, _clock) = i2c1_i2c2_led_delay::setup_from_dp(dp);
 
-    let manager = shared_bus::BusManagerSimple::new(i2c);
-    let interface = I2CDisplayInterface::new(manager.acquire_i2c());
+    let interface = I2CDisplayInterface::new(i2c1);
     let mut display = Ssd1306::new(interface, DisplaySize128x32, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
     display.init().unwrap();
@@ -61,7 +62,7 @@ fn main() -> ! {
         .text_color(BinaryColor::On)
         .build();
 
-    let mut sensor = Tcs3472::new(manager.acquire_i2c());
+    let mut sensor = Tcs3472::new(i2c2);
     sensor.enable().unwrap();
     sensor.enable_rgbc().unwrap();
     while !sensor.is_rgbc_status_valid().unwrap() {
