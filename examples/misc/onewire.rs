fb33ba653824198d1e23c243249815c3b5bc7f51
Paul Gilbert <pdgilbert@users.noreply.github.com>
2025-05-07 00:23:58 -0300

    onewire compiling but untested.

--- examples/misc/onewire.rs
+++ examples/misc/onewire.rs
@@ -1,7 +1,6 @@
-//  NOT COMPILING   NEEDS WORK  // CHECK  katyo has an eh1 branch
-
-// example from crate onewire README
 //read the temperature from a DS18B20. 
+// example modified from crate onewire README (https://github.com/kellerkindt/onewire)
+//  Compiling but untested and NEEDS WORK 
 
 #![deny(unsafe_code)]
 #![no_std]
@@ -20,52 +19,17 @@ use panic_halt as _;
 
 use cortex_m_rt::entry;
 
-use embedded_hal::digital::{InputPin, OutputPin};
 use embedded_hal::delay::DelayNs;
 use onewire;
-
-
-//#[cfg(feature = "stm32f1xx")] //  eg blue pill stm32f103
-//use stm32f1xx_hal::{
-//    timer::SysDelay as Delay,
-//    gpio::{gpioc::PC13, Output, PushPull},
-//    pac::{CorePeripherals, Peripherals},
-//    prelude::*,
-//};
-//
-//#[cfg(feature = "stm32f1xx")]
-//fn setup() -> (PC13<Output<PushPull>>, Delay) {
-//    let cp = CorePeripherals::take().unwrap();
-//    let p = Peripherals::take().unwrap();
-//    let rcc = p.RCC.constrain();
-//    let clocks = rcc.cfgr.freeze(&mut p.FLASH.constrain().acr);
-//    let mut gpioc = p.GPIOC.split();
-//
-// //    impl LED for PC13<Output<PushPull>> {
-// //        fn on(&mut self) -> () {
-// //            self.set_low()
-// //        }
-// //        fn off(&mut self) -> () {
-// //            self.set_high()
-// //        }
-// //    }
-//
-//    // see examples in https://github.com/stm32-rs/stm32f1xx-hal/examples/
-//    //  for other (better) ways to do delay
-//
-//    // return tuple  (led, delay)
-//    (
-//        gpioc.pc13.into_push_pull_output(&mut gpioc.crh), // led on pc13 with on/off
-//        cp.SYST.delay(&clocks)
-//    )
-//}
+use onewire::OpenDrainOutput;
 
 use rust_integration_testing_of_examples::setup;
-use rust_integration_testing_of_examples::setup::{Peripherals, Delay};
+use rust_integration_testing_of_examples::setup::{Peripherals};
 
+#[entry]
 fn main() -> ! {
-//    let cp = CorePeripherals::take().unwrap();
-    let (one, mut delay) = setup::pin_delay();
+    let dp = Peripherals::take().unwrap();
+    let (mut one, mut delay) = setup::pin_delay_from_dp(dp);
 
     delay.delay_ms(1000);
     
@@ -73,11 +37,11 @@ fn main() -> ! {
 
     //let mut one = gpioa
     //    .pa8
-    //    .into_open_drain_output(&mut gpioa.crh);
-    //    //.downgrade();   CHECK WHAT THIS WAS SUPPOSED TO DO
-    
-     // Pulling the pin high to avoid confusing the sensor when initializing.
-     //dht.set_high();
+    //    .into_open_drain_output(&mut gpioa.crh)
+    //    .downgrade();   //CHECK WHAT THIS WAS SUPPOSED TO DO
+   
+    // Pulling the pin high to avoid confusing the sensor when initializing. NOT SURE IF THIS IS NEEDED
+    let _ = one.set_high();
     
     let mut wire = onewire::OneWire::new(&mut one, false);
     
@@ -91,7 +55,7 @@ fn main() -> ! {
     while let Some(device) = wire.search_next(&mut search, &mut delay).unwrap() {
         match device.address[0] {
             onewire::ds18b20::FAMILY_CODE => {
-                let mut ds18b20 = onewire::DS18B20::new(device).unwrap();
+                let ds18b20 = onewire::DS18B20::new(device).unwrap();
                 
                 // request sensor to measure temperature
                 let resolution = ds18b20.measure_temperature(&mut wire, &mut delay).unwrap();
@@ -101,7 +65,7 @@ fn main() -> ! {
                 //delay.delay(resolution.millis());
                 
                 // read temperature
-                let temperature = ds18b20.read_temperature(&mut wire, &mut delay).unwrap();
+                let _temperature = ds18b20.read_temperature(&mut wire, &mut delay).unwrap();
             },
             _ => {
                 // unknown device type            
